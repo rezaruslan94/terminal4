5d16669da20b40e1b2b6853cfb21eda1d2d41331
rezaruslan94 <reza.ruslan94@gmail.com>
2017-01-18 10:25:06 +0700

    Error Dynamic Fields

--- app/models/division.rb
+++ app/models/division.rb
@@ -2,4 +2,8 @@ class Division < ApplicationRecord
   belongs_to :employe
   belongs_to :department
   has_many :areas
+  has_many :a, class_name: "DivisionField"
+
+  
+  accepts_nested_attributes_for :a, allow_destroy: true
 end
