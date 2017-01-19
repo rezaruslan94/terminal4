da13ebb39dea9c91f3fa3f7be982ebf807b9db91
rezaruslan94 <reza.ruslan94@gmail.com>
2017-01-19 10:11:44 +0700

    Error Fix Dynamic Field Division

--- app/models/division.rb
+++ app/models/division.rb
@@ -2,7 +2,7 @@ class Division < ApplicationRecord
   belongs_to :employe
   belongs_to :department
   has_many :areas, inverse_of: :division
-  validates_uniqueness_of :number
+  
 
   accepts_nested_attributes_for :areas, allow_destroy: true
 end
